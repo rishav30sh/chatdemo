3386b7dabb91730166f98bcbbe5dc98915538400
Rishav <rishav30sharma@gmail.com>
2019-04-16 11:29:48 +0530

    Updates

---
+++ precache-manifest.3622d699095c718702ddad30bfef9242.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c06cf744b70c6c8a699e",
+    "url": "/chatdemo/static/js/runtime~main.67afe920.js"
+  },
+  {
+    "revision": "4bd30d863c5270f117db",
+    "url": "/chatdemo/static/js/main.51bc183e.chunk.js"
+  },
+  {
+    "revision": "0b678a5c1c9bf799926d",
+    "url": "/chatdemo/static/js/2.afbce266.chunk.js"
+  },
+  {
+    "revision": "4bd30d863c5270f117db",
+    "url": "/chatdemo/static/css/main.328bdb55.chunk.css"
+  },
+  {
+    "revision": "4d52d02d03088814f7ff73d32ccc2d67",
+    "url": "/chatdemo/index.html"
+  }
+];
